641b38947e4d99e094b49889ccc3eb37a0dbcbcb
sitepinjames <sitepinjames@gmail.com>
2025-11-10 23:20:34 -0500

    locals.tf

--- locals.tf
+++ locals.tf
@@ -1,18 +1,18 @@
 locals {
   ecs_tags = {
-    Environment  = "Dev"
+    Environment  = "Devlp"
     Project      = "Codechalllenge"
     ResourceType = "ecs"
   }
 
   alb_tags = {
-    Environment  = "Dev"
+    Environment  = "Devlp"
     Project      = "Codechallenge"
     ResourceType = "elb"
   }
 
   S3_tags = {
-    Environment  = "Dev"
+    Environment  = "Devlp"
     Project      = "Codechallenge"
     ResourceType = "S3"
   }
