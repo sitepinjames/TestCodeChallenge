44a9d71293c18d9363a4723fbd521221f7e76596
sitepinjames <sitepinjames@gmail.com>
2025-11-10 20:05:04 -0500

    testverif

--- modules/testverif/main.tf
+++ modules/testverif/main.tf
@@ -1,12 +1,12 @@
-variable "expected_image_tag" {
-  type = string
-}
+# variable "expected_image_tag" {
+#   type = string
+# }
 
-variable "actual_image_tag" {
-  type = string
-}
-output "image_tag_match" {
-  value = var.expected_image_tag == var.actual_image_tag
-}
+# variable "actual_image_tag" {
+#   type = string
+# }
+# output "image_tag_match" {
+#   value = var.expected_image_tag == var.actual_image_tag
+# }
 
 
